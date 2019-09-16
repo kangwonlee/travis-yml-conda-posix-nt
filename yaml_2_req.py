dfc4423683391d4585c768dc10823e68c51152a2
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-09-16 23:21:51 +0900

    added yaml_to_req

--- yaml_2_req.py
+++ yaml_2_req.py
@@ -1,6 +1,11 @@
+# https://pip.pypa.io/en/stable/reference/pip_install/
+# https://pyyaml.org/wiki/PyYAMLDocumentation
 import argparse
 import os
+import re
 import sys
+
+import yaml
 
 
 def main(argv=sys.argv):
@@ -8,7 +13,6 @@ def main(argv=sys.argv):
 
 
 def get_argparse():
-
     p = argparse.ArgumentParser()
 
     p.add_argument('--yaml', type=str, default='tests', help='folder')
@@ -16,6 +20,26 @@ def get_argparse():
     p.add_argument('--req', type=str, default='req', help='folder')
 
     return p
+
+
+def yaml_to_req(yaml_txt):
+    p = yaml.load(yaml_txt)
+
+    req_txt = ''
+
+    if 'name' in p:
+        req_txt += f"# name : {p['name']}\n"
+
+    if 'channels' in p:
+        req_txt += "# channels :\n"
+        for ch in p['channels']:
+            req_txt += f"# {ch}\n"
+
+    for lib in p['dependencies']:
+        lib_replaced = re.sub(r'\w(\=)\d', ' == ', lib)
+        req_txt += f"{lib_replaced}\n"
+
+    return req_txt
 
 
 if "__main__" == __name__:
